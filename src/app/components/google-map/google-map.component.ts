fe659779683e98718533de068f758c165fb61b9e
Shrey Pandya <shrey150@yahoo.com>
2019-03-13 11:29:40 -0400

    added map to component

--- src/app/components/google-map/google-map.component.ts
+++ src/app/components/google-map/google-map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+
+declare var google;
 
 @Component({
   selector: 'google-map',
@@ -7,8 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoogleMapComponent implements OnInit {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    console.log('hi');
+
+    const latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
   constructor() { }
 
-  ngOnInit() {}
 
 }
